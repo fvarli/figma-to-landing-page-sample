33694837b3dbbcb627c11d0ba4471f4729c9abca
Ferzender Varli <fvarlii@gmail.com>
2025-02-11 16:26:34 +0300

    navbar section updated

--- components/header.php
+++ components/header.php
@@ -1,19 +1,35 @@
-<header class="navbar navbar-expand-lg navbar-light bg-light">
+<header class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
     <div class="container">
+        <!-- Logo -->
         <a class="navbar-brand" href="/">
             <img src="assets/images/gemak-logo.png" alt="Logo" height="67">
         </a>
+
+        <!-- Mobil Menü Butonu -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <nav class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+
+        <!-- Menü İçeriği -->
+        <nav class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="#">Anasayfa</a></li>
+                <li class="nav-item"><a class="nav-link" href="#faaliyetler">Faaliyetler</a></li>
                 <li class="nav-item"><a class="nav-link" href="#inovasyon">İnovasyon</a></li>
                 <li class="nav-item"><a class="nav-link" href="#kariyer">Kariyer</a></li>
                 <li class="nav-item"><a class="nav-link" href="#medya">Medya</a></li>
                 <li class="nav-item"><a class="nav-link" href="#iletisim">İletişim</a></li>
             </ul>
+
+            <!-- Arama İkonu -->
+            <img src="assets/images/icons/search.svg" alt="Search" height="18" class="ms-3 d-none d-lg-inline">
+
+            <!-- Dil Seçimi -->
+            <div class="d-flex align-items-center ms-3">
+                <p class="mb-0 me-2 text-uppercase">TR</p>
+                <div class="vr"></div>
+                <p class="mb-0 ms-2 text-uppercase">ENG</p>
+            </div>
         </nav>
     </div>
 </header>
