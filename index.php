b7b65335063294db7aa001c2e3b1883d535918fd
Ferzender Varli <fvarlii@gmail.com>
2025-02-11 15:35:45 +0300

    experience section updated

--- index.php
+++ index.php
@@ -19,8 +19,90 @@
     </section>
 
     <!-- Experience Section -->
-    <section>
-        Experience Section
+    <section class="experience-section pt-5 pb-5">
+        <div class="container">
+            <!-- Başlık ve Açıklama -->
+            <div class="row col-lg-6">
+                <h2 class="fw-bold fs-1">50 Yıllık Deneyim</h2>
+                <p>Yarım asrı aşkın bir süredir, gemi inşası ve denizcilik sektöründe uzmanlaşarak küresel ölçekte güvenilir projelere imza atıyoruz.</p>
+            </div>
+
+            <!-- Bilgiler Grid Sistemi -->
+            <div class="row pt-5 pb-5">
+                <div class="col-md-4 col-sm-6">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/muhendis.svg" alt="260 Mühendis">
+                        <h3 class="pt-3 fw-bold fs-2">260 Mühendis</h3>
+                        <p>Projelerimizi planlayan ve hayata geçiren uzman mühendislerimizle, ileri teknolojilerle çalışıyoruz.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/calisan.svg" alt="900 Çalışan">
+                        <h3 class="pt-3 fw-bold fs-2">900 Çalışan</h3>
+                        <p>Üretim, bakım ve onarım süreçlerimizin her aşamasında yer alan deneyimli bir ekibimiz var.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/is-gucu.svg" alt="2500 İş Gücü">
+                        <h3 class="pt-3 fw-bold fs-2">2500 İş Gücü</h3>
+                        <p>Büyük projelerin ihtiyaçlarını karşılayabilecek kapsamlı bir iş gücü altyapısına sahibiz.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/agirlik.svg" alt="82250 Ton">
+                        <h3 class="pt-3 fw-bold fs-2">82250 Ton</h3>
+                        <p>Son beş yılda işlenen toplam çelik miktarıyla sektördeki liderliğimizi kanıtlıyoruz.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/agirlik.svg" alt="3350 Ton">
+                        <h3 class="pt-3 fw-bold fs-2">3350 Ton</h3>
+                        <p>Son beş yılda, farklı projeler için işlenmiş boru miktarı ile hassas mühendislik örnekleri sunuyoruz.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/gemi.svg" alt="Gemi">
+                        <h3 class="pt-3 fw-bold fs-2">2800 Gemi</h3>
+                        <p>Güçlü altyapımız sayesinde binlerce gemiye bakım ve onarım hizmeti verdik.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/gemi-insasi.svg" alt="Gemi İnşası">
+                        <h3 class="pt-3 fw-bold fs-2">57 Gemi İnşası</h3>
+                        <p>1985’ten günümüze, her biri özenle tasarlanmış ve inşa edilmiş 57 gemi ürettik.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/donusum.svg" alt="Dönüşüm">
+                        <h3 class="pt-3 fw-bold fs-2">23 Dönüşüm</h3>
+                        <p>1994’ten itibaren müşterilerimizin ihtiyaçlarına göre modernize edilen 23 gemi teslim ettik.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 pt-5">
+                    <div class="experience-box">
+                        <img src="assets/images/experiences/offshore.svg" alt="Offshore Hizmeti">
+                        <h3 class="pt-3 fw-bold fs-2">17 Offshore Hizmeti</h3>
+                        <p>1999’dan bugüne kadar offshore sektöründe verdiğimiz uzman hizmetlerle sektörde fark yarattık.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </section>
 
     <!-- Facility Highlights Section -->
