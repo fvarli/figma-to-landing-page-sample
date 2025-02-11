89c47f898f8d1903304617efd0809806fd1eb8a5
Ferzender Varli <fvarlii@gmail.com>
2025-02-11 16:39:44 +0300

    navbar section updated for responsive

--- components/header.php
+++ components/header.php
@@ -1,9 +1,16 @@
 <header class="navbar navbar-expand-lg header bg-white shadow-sm">
-    <div class="container header__top">
+    <div class="container-fluid header__top">
         <!-- Logo -->
         <a class="navbar-brand" href="/">
             <img src="assets/images/gemak-logo.png" alt="Logo" class="header__logo">
         </a>
+
+        <div class="d-flex align-items-center d-lg-none">
+            <img src="assets/images/icons/search.svg" alt="Search" class="header__search me-3">
+            <p class="mb-0 header__language">TR</p>
+            <div class="header__divider"></div>
+            <p class="mb-0 header__language ms-2">ENG</p>
+        </div>
 
         <!-- Mobil Menü Butonu -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -11,7 +18,7 @@
         </button>
 
         <!-- Menü İçeriği -->
-        <nav class="collapse navbar-collapse d-flex justify-content-between align-items-center" id="navbarNav">
+        <nav class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav header__menu mx-auto">
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#">Anasayfa</a></li>
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#faaliyetler">Faaliyetler</a></li>
@@ -21,12 +28,8 @@
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#iletisim">İletişim</a></li>
             </ul>
 
-            <!-- Sağ Taraf (Arama + Dil Seçimi) -->
-            <div class="d-flex align-items-center">
-                <!-- Arama İkonu -->
+            <div class="d-flex align-items-center d-none d-lg-flex">
                 <img src="assets/images/icons/search.svg" alt="Search" class="header__search me-3">
-
-                <!-- Dil Seçimi -->
                 <p class="mb-0 header__language">TR</p>
                 <div class="header__divider"></div>
                 <p class="mb-0 header__language ms-2">ENG</p>
