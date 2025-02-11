ccb4a043854bb069e738c686163f3372e529a7bf
Ferzender Varli <fvarlii@gmail.com>
2025-02-11 16:42:13 +0300

    navbar section updated for responsive

--- components/header.php
+++ components/header.php
@@ -5,13 +5,6 @@
             <img src="assets/images/gemak-logo.png" alt="Logo" class="header__logo">
         </a>
 
-        <div class="d-flex align-items-center d-lg-none">
-            <img src="assets/images/icons/search.svg" alt="Search" class="header__search me-3">
-            <p class="mb-0 header__language">TR</p>
-            <div class="header__divider"></div>
-            <p class="mb-0 header__language ms-2">ENG</p>
-        </div>
-
         <!-- Mobil Menü Butonu -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
@@ -26,8 +19,19 @@
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#kariyer">Kariyer</a></li>
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#medya">Medya</a></li>
                 <li class="nav-item"><a class="nav-link header__menu-item" href="#iletisim">İletişim</a></li>
+
+                <!-- Mobilde Görünecek Arama ve Dil Seçimi -->
+                <li class="nav-item d-lg-none text-center mt-3">
+                    <img src="assets/images/icons/search.svg" alt="Search" class="header__search">
+                </li>
+                <li class="nav-item d-lg-none text-center mt-2">
+                    <p class="header__language d-inline">TR</p>
+                    <div class="header__divider d-inline-block"></div>
+                    <p class="header__language d-inline ms-2">ENG</p>
+                </li>
             </ul>
 
+            <!-- Desktop için Arama ve Dil Seçimi -->
             <div class="d-flex align-items-center d-none d-lg-flex">
                 <img src="assets/images/icons/search.svg" alt="Search" class="header__search me-3">
                 <p class="mb-0 header__language">TR</p>
