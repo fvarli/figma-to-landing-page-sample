6a4b795748c766c7f02524a0f67b28fd15433214
Ferzender Varli <fvarlii@gmail.com>
2025-02-11 14:29:41 +0300

    created sections

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-<?php
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<?php include 'components/header.php'; ?>
+
+<main>
+
+    <!-- Hero Banner Section -->
+    <section>
+        Hero Banner Section
+    </section>
+
+    <!-- Experience Section -->
+    <section>
+        Experience Section
+    </section>
+
+    <!-- Facility Highlights Section -->
+    <section>
+        Facility Highlights Section
+    </section>
+
+    <!-- Innovation Section -->
+    <section>
+        Innovation Section
+    </section>
+
+    <!-- Latest News Section -->
+    <section>
+        Latest News Section
+    </section>
+
+    <!-- Contact Section -->
+    <section>
+        Contact Section
+    </section>
+
+</main>
+
+<?php include 'components/footer.php'; ?>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
